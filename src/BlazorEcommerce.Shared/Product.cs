2d95391ff544b1d8d8e04b39f34d7458da7cf129
LilianBittar <bittarlily@gmail.com>
2023-01-24 22:31:17 +0100

    Add category model.

--- src/BlazorEcommerce.Shared/Product.cs
+++ src/BlazorEcommerce.Shared/Product.cs
@@ -10,4 +10,6 @@ public class Product
     public string ImageUrl { get; set; } = string.Empty;
     [Column(TypeName = "decimal(18,2)")]
     public decimal Price { get; set; }
+    public Category? Category { get; set; }
+    public int CategoryId { get; set; }
 }
