38d6c73de58406e08e23d5afd9a146731bf92b3a
Maxim Zavidei <maxim-zavidei+git@pm.me>
2023-01-29 20:50:33 +0100

    added get product by category on serva

--- src/Server/Services/ProductService/ProductService.cs
+++ src/Server/Services/ProductService/ProductService.cs
@@ -38,4 +38,16 @@ public class ProductService : IProductService
         }
         return response;
     }
+
+    public async Task<ServiceResponse<List<Product>>> GetProductsByCategory(string categoryUrl)
+    {
+        var response = new ServiceResponse<List<Product>>
+        {
+            Data = await _context.Products
+                .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+                .ToListAsync()
+        };
+
+        return response;
+    }
 }
