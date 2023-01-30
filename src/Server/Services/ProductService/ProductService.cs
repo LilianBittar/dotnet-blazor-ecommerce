77c10b8e8302850c9eb41c625c40fed61638d57d
LilianBittar <bittarlily@gmail.com>
2023-01-30 19:27:52 +0100

    Include product variants.

--- src/Server/Services/ProductService/ProductService.cs
+++ src/Server/Services/ProductService/ProductService.cs
@@ -17,7 +17,7 @@ public class ProductService : IProductService
     {
         var response = new ServiceResponse<List<Product>>
         {
-            Data = await _context.Products.ToListAsync()
+            Data = await _context.Products.Include(p => p.Variants).ToListAsync()
         };
 
         return response;    
@@ -26,7 +26,10 @@ public class ProductService : IProductService
     public async Task<ServiceResponse<Product>> GetProductsAsync(int productId)
     {
         var response = new ServiceResponse<Product>();
-        var product = await _context.Products.FindAsync(productId);
+        var product = await _context.Products
+        .Include(p => p.Variants)
+        .ThenInclude(v => v.ProductType)
+        .FirstOrDefaultAsync(p => p.Id == productId);
         if (product == null)
         {
             response.Success = false;
@@ -45,6 +48,7 @@ public class ProductService : IProductService
         {
             Data = await _context.Products
                 .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+                .Include(p => p.Variants)
                 .ToListAsync()
         };
 
