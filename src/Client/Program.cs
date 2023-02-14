8bd0c3a9ecb4e2352300fd0f010399461181de99
LilianBittar <bittarlily@gmail.com>
2023-02-14 16:32:39 +0100

    Implemented Cart service.

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -12,6 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddBlazoredLocalStorage();
 await builder.Build().RunAsync();
 
