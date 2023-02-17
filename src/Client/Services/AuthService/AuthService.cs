5988207fcfe9b8a33ac96b9a3066ecd9442ee4be
Lilian Bittar <bittarlily@gmail.com>
2023-02-17 21:16:07 +0100

    Added Auth service.

---
+++ src/Client/Services/AuthService/AuthService.cs
@@ -0,0 +1,6 @@
+namespace Client.Services.AuthService;
+
+public class AuthService : IAuthService
+{
+    
+}
---
+++ src/Client/Services/AuthService/IAuthService.cs
@@ -0,0 +1,6 @@
+namespace Client.Services.AuthService;
+
+public interface IAuthService
+{
+    
+}
