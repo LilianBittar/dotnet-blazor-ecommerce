b3fc90575d52cac052a0385f92b67edc66127262
Lilian Bittar <bittarlily@gmail.com>
2023-02-21 15:09:28 +0100

    Implemented store cart items method.

--- src/Server/Services/CartService/CartService.cs
+++ src/Server/Services/CartService/CartService.cs
@@ -55,4 +55,15 @@ public class CartService : ICartService
         }
         return result;
     }
+
+    public async Task<ServiceResponse<List<CartProductResponse>>> StoreCartItems(List<CartItem> cartItems, int userId)
+    {
+        cartItems.ForEach(cartItem => cartItem.UserId = userId);
+        _context.CartItems.AddRange(cartItems);
+        await _context.SaveChangesAsync();
+
+        return await GetCartProducts(
+            await _context.CartItems
+            .Where(ci => ci.UserId == userId).ToListAsync());
+    }
 }
--- src/Server/Services/CartService/ICartService.cs
+++ src/Server/Services/CartService/ICartService.cs
@@ -5,4 +5,6 @@ namespace Server.Services.CartService;
 public interface ICartService
 {
     Task<ServiceResponse<List<CartProductResponse>>> GetCartProducts(List<CartItem> cartItems);
+    Task<ServiceResponse<List<CartProductResponse>>> StoreCartItems(List<CartItem> cartItems, int userId);
+
 }
