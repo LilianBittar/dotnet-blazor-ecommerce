04af7a764177225664d1e2002e9fb7598fadeb0c
Lilian Bittar <bittarlily@gmail.com>
2023-03-06 00:32:03 +0100

    Created order overview class

--- src/BlazorEcommerce.Shared/Order.cs
+++ src/BlazorEcommerce.Shared/Order.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BlazorEcommerce.Shared;
 
 public class Order
@@ -5,6 +7,8 @@ public class Order
     public int Id { get; set; }
     public int UserId { get; set; }
     public DateTime OrderDate { get; set; } = DateTime.Now;
+
+    [Column(TypeName = "decimal(18,2)")]
     public decimal TotalPrice { get; set; }
     public List<OrderItem> OrderItems { get; set; }
 }
